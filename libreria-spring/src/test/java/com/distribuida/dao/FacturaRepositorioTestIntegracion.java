a9ad232616cf5a74a636823ffca3c2ec169202eb
ismac-librer-a-nocturno2025 <andrescapitan2005@gmail.com>
2025-06-09 21:24:04 -0500

    migraciopn de libreria ty libreria-spring a proyecto libreria

--- libreria-spring/src/test/java/com/distribuida/dao/FacturaRepositorioTestIntegracion.java
+++ libreria-spring/src/test/java/com/distribuida/dao/FacturaRepositorioTestIntegracion.java
@@ -2,6 +2,7 @@
 
 
 package com.distribuida.dao;
+import com.distribuida.model.Cliente;
 import com.distribuida.model.Factura;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -23,16 +28,68 @@ public class FacturaRepositorioTestIntegracion {
       @Autowired
     private FacturaRepositorio facturaRepositorio;
 
+
+@Autowired
+private ClienteRepositorio clienteRepositorio;
+
       @Test
     public void finAll(){
 
           List<Factura> facturas = facturaRepositorio.findAll();
+          assertNotNull(facturas);
+          assertTrue(facturas.size()>0);
           for (Factura item : facturas){
 
               System.out.println(item.toString());
 
           }
       }
+      @Test
+    public void findOne(){
 
+          Optional<Factura> facturaExistente = facturaRepositorio.findById(1);
+          assertNotNull(facturaExistente.isPresent());
+          assertEquals("FAC-0001",facturaExistente.orElse(null).getNumFactura(), "La factura deberia existir");
+
+          assertEquals(134.79,facturaExistente.orElse(null).getTotalNeto());
+
+
+          System.out.println(facturaExistente.toString());
+
+      }
+
+      @Test
+    public void save(){
+          Optional<Cliente> cliente = clienteRepositorio.findById(1);
+       Factura factura = new Factura(0,"FAC-0099", new Date(), 100.00, 15.00,115.00,cliente.orElse(null));
+       facturaRepositorio.save(factura);
+
+      }
+@Test
+    public void update(){
+          Optional<Factura> factura = facturaRepositorio.findById(86);
+          Optional<Cliente> cliente = clienteRepositorio.findById(2);
+          factura.orElse(null).setNumFactura("FAC-00100");
+    factura.orElse(null).setFecha(new Date());
+    factura.orElse(null).setTotalNeto(200.00);
+    factura.orElse(null).setIva(15.00);
+    factura.orElse(null).setTotal(230.00);
+    factura.orElse(null).setCliente(cliente.orElse(null));
+
+    facturaRepositorio.save(factura.orElse(null));
+
+
+
+
+}
+@Test
+
+    public void delete(){
+
+          if (facturaRepositorio.existsById(86)){
+              facturaRepositorio.deleteById(86);
+          }
+
+}
 
 }
