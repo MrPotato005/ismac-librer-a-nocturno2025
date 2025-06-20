19f36538217bfcfc4dae524273c5ab3770bb8387
ismac-librer-a-nocturno2025 <andrescapitan2005@gmail.com>
2025-06-19 21:24:01 -0500

    andres

---
+++ libreria-spring/src/test/java/com/distribuida/controller/ClienteControllerTestUnitaria.java
@@ -0,0 +1,99 @@
+package com.distribuida.controller;
+
+import com.distribuida.model.Cliente;
+import com.distribuida.service.ClienteService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ClienteControllerTestUnitaria {
+
+    @InjectMocks
+
+    private ClienteController clienteController;
+
+
+    @Mock
+    private ClienteService clienteService;
+    private Cliente cliente;
+
+     @BeforeEach
+    public void setUp(){
+
+         MockitoAnnotations.openMocks(this);
+        cliente = new Cliente();
+        cliente.setId_cliente(1);
+         cliente.setCedula("1753435153");
+         cliente.setNombre("Andres");
+         cliente.setApellido("Taipe");
+         cliente.setDirreccion("Av. por ahy");
+         cliente.setTelefono("02623532626");
+         cliente.setCorreo("jtaipe@correo.com");
+     }
+
+
+
+     @Test
+    public void testFindAll(){
+
+     when(clienteService.findAll()).thenReturn(List.of(cliente));
+         ResponseEntity<List<Cliente>> respuesta = clienteController.findAll();
+         assertEquals(200,respuesta.getStatusCodeValue());
+         assertEquals(1,respuesta.getBody().size());
+         verify(clienteService,times(1)).findAll();
+
+     }
+
+
+     @Test
+    public void testFindOneExistente(){
+         when(clienteService.findOne(1)).thenReturn(cliente);
+         ResponseEntity<Cliente> respuesta = clienteController.findOne(1);
+         assertEquals(200,respuesta.getStatusCodeValue());
+         assertEquals(cliente.getNombre(),respuesta.getBody().getNombre());
+         verify(clienteService,times(1)).findOne(1);
+
+
+
+     }
+
+
+     @Test
+    public void testFindOneNoExistente(){
+                   when(clienteService.findOne(2)).thenReturn(null);
+                   ResponseEntity<Cliente> respuesta = clienteController.findOne(2);
+                   assertEquals(404,respuesta.getStatusCodeValue());
+                  // verify(clienteService,times(0)).findOne(2);
+
+     }
+
+     @Test
+
+    public  void testSave(){
+when(clienteService.save(any(Cliente.class))).thenReturn(cliente);
+ResponseEntity<Cliente> respuesta = clienteController.save(cliente);
+assertEquals(200,respuesta.getStatusCodeValue());
+assertEquals("Andres",respuesta.getBody().getNombre());
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+}
